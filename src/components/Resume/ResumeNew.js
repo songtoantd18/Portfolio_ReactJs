49c3da0a0bbcea4503e8e023536518b632ce92a7
songtoantd18 <songtoantd18@gmail.com>
2023-03-26 19:30:17 +0700

    replace CV ok

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/NGUYEN SONG TOAN-CV-AUTOMATIC-PRODUCTION.pdf";
+import pdf from "../../Assets/../Assets/NGUYEN SONG TOAN-CV-AUTOMATIC-PRODUCTION1.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/songtoantd18/Portfolio_ReactJs/master/src/Assets/NGUYEN%20SONG%20TOAN-CV-AUTOMATIC-PRODUCTION.pdf";
+  "https://raw.githubusercontent.com/songtoantd18/Portfolio_ReactJs/master/src/Assets/NGUYEN%20SONG%20TOAN-CV-AUTOMATIC-PRODUCTION1.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
